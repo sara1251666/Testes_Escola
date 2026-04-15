da65c266b222127f36c9792b70256acdd4b2ccf9
Sara Londreira <1251666@isep.ipp.pt>
2026-04-15 18:43:23 +0100

    Delete LP2-Grupo1-quality directory

--- LP2-Grupo1-quality/LP2-Grupo1/src/model/Gestor.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Gestor extends Utilizador {
-
-    // ---------- ATRIBUTOS ESPECÍFICOS ----------
-    public Gestor(String email, String password, String nome, String nif, String morada, String dataNascimento) {
-        super(email, password, nome, nif, morada, dataNascimento);
-    }
-}
