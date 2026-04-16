e62c2a8a81e1f8acf4e66cee6d442ac894db5623
Sara Londreira <1251666@isep.ipp.pt>
2026-04-16 09:27:15 +0100

    Delete ISSMF/ISSMF directory

--- ISSMF/ISSMF/src/Main.java
+++
@@ -1,8 +0,0 @@
-import view.MainView;
-
-public class Main {
-    public static void main(String[] args) {
-        MainView menuPrincipal = new MainView();
-        menuPrincipal.iniciar();
-    }
-}
--- ISSMF/ISSMF/src/common/OperacaoCanceladaException.java
+++
@@ -1,7 +0,0 @@
-package common;
-
-public class OperacaoCanceladaException extends RuntimeException {
-    public OperacaoCanceladaException() {
-        super("A operação foi cancelada pelo utilizador.");
-    }
-}
